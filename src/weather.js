16c4fd9dc07debc882b24a304b8ab0bc26158ffa
CodePioneer2 <166389859+CodePioneer2@users.noreply.github.com>
2025-01-05 11:45:45 +0200

    setup weather model

--- src/weather.js
+++ src/weather.js
@@ -1,8 +1,17 @@
-const Weather = (location) => {
+const Weather = (() => {
   const API_KEY = 'F4KGLLYBJRFAT4GYW6B5DVKSM';
+  const location = 'Turku';
+  const API_ADDRESS = `https://weather.visualcrossing.com/VisualCrossingWebServices/rest/services/timeline/${location}?unitGroup=metric&key=${API_KEY}&contentType=json`;
 
-  const getData = () => {};
+  const getData = async () => {
+    const fetchData = await fetch(API_ADDRESS);
+    const fetchObject = await fetchData.json();
+    const result = fetchObject;
+    return result;
+  };
+
+  
   return { getData };
-};
+})();
 
 export default Weather;
